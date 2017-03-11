4b1c3b45fe60f2826e37d3e7517f87cda4273349
Johan <johaa1993@gmail.com>
2017-03-12 00:40:08 +0100

    test

--- src/main_ztest.adb
+++ src/main_ztest.adb
@@ -3,6 +3,7 @@ with Ada.Streams;
 with Interfaces.C;
 with Ada.Text_IO;
 with Ada.Integer_Text_IO;
+with Home_Pictures.PNG;
 
 procedure Main_ZTest is
 
@@ -10,31 +11,40 @@ procedure Main_ZTest is
    use Ada.Integer_Text_IO;
    use Ada.Streams;
    use ztest;
+   use Home_Pictures.PNG;
    use type Interfaces.C.unsigned;
 
-   Z : Z_Stream;
-   Data_Compressed : Stream_Element_Array := (16#18#, 16#57#, 16#63#, 16#0#, 16#82#, 16#FF#, 16#F5#, 16#F5#, 16#F5#, 16#FF#, 16#F#, 16#1F#, 16#3E#, 16#FC#, 16#9F#, 16#11#, 16#C4#, 16#1#, 16#89#, 16#1C#, 16#3E#, 16#7C#, 16#98#, 16#1#, 16#0#, 16#88#, 16#17#, 16#A#, 16#D#);
-   Data_Uncompressed : Stream_Element_Array (1 .. 1000);
-   R : Z_Code;
+   Z : Z_Native_Stream;
+   Data_Compressed : Stream_Element_Array := (16#18#, 16#57#, 16#63#, 16#0#, 16#82#, 16#FF#, 16#F5#, 16#F5#, 16#F5#, 16#FF#, 16#F#, 16#1F#, 16#3E#, 16#FC#, 16#9F#, 16#9#, 16#C4#, 16#3#, 16#1#, 16#5B#, 16#5B#, 16#5B#, 16#6#, 16#38#, 16#87#, 16#81#, 16#81#, 16#81#, 16#1#, 16#0#, 16#DB#, 16#EA#, 16#7#, 16#7F#);
+   Data_Uncompressed : Stream_Element_Array (1 .. 3 * 3 * 4);
+   Data_Uncompressed2 : Stream_Element_Array (1 .. 1000);
+
+
+   Status : Z_Status;
 
 begin
+   New_Line (2);
+   Initialize_Inflate (Z, 15);
+   Set_Next_Input (Z, Data_Compressed);
+   Inflate_All (Z, 3, 3, 4, Data_Uncompressed);
+
+   for E of Data_Uncompressed loop
+      Put (Integer (E), 8, 16);
+   end loop;
+
+   New_Line (2);
 
    Initialize_Inflate (Z, 15);
-
-   Z.Next_In := Data_Compressed'Address;
-   Z.Avail_In := Data_Compressed'Length;
-
-   Z.Next_Out := Data_Uncompressed'Address;
-   Z.Avail_Out := Data_Uncompressed'Length;
-
-   R := Inflate (Z, Z_Flush_None);
-   Put_Line (R'Img);
-   Put_Line (Z.Avail_Out'Img);
-   Put_Line (Z.Total_Out'Img);
-
-   for E of Data_Uncompressed (1 .. Stream_Element_Offset (Z.Total_Out)) loop
+   Set_Next_Input (Z, Data_Compressed);
+   Set_Next_Output (Z, Data_Uncompressed2);
+   Status := Inflate (Z, Z_Flush_None);
+   Put_Line (Status'Img);
+   Put_Line (Z.Output_Available'Img);
+   Put_Line (Z.Output_Total'Img);
+   for E of Data_Uncompressed2 (1 .. Stream_Element_Offset (Z.Output_Total)) loop
       Put (Integer (E), 8, 16);
    end loop;
    New_Line;
+
 
 end Main_ZTest;
